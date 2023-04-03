470c1d5238f6546584df18bce55c042a7052bd0d
HugogomesM <hugowork121@gmail.com>
2023-04-03 02:04:19 -0300

    criaçao de modelo

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,23 +1,19 @@
 const pokemonList = document.getElementById("pokemonList");
 
-function convertTypesToLi(pokemonTypes) { 
-    return pokemonTypes.map((typeSlots)=> `<li class="type">${typeSlots.type.name}</li>`)
-}
-
 function convertPokemonToLi(pokemon) {
     return `
                 <li class="pokemon">
 
-                <span class="number">#${pokemon.id}</span>
+                <span class="number">#${pokemon.number}</span>
                 <span class="name">${pokemon.name}</span>
                 <div class="detail">
 
                     <ol class="types">
-                        ${convertTypesToLi(pokemon.types).join("")}
+                        ${pokemon.types.map((type)=> `<li class="type">${type}</li>`).join("")}
                         
                     </ol>
 
-                    <img src="${pokemon.sprites.other.dream_world.front_default}"
+                    <img src="${pokemon.photo}"
                         alt="${pokemon.name}">
 
                 </div>
--- pokedex/assets/js/poke-api.js
+++ pokedex/assets/js/poke-api.js
@@ -1,7 +1,23 @@
 const pokeApi = {}
 
+function convertPokemonDetailsToPokemonModel(pokemonDetails) {
+    const pokemon1 = new pokemon()
+    pokemon1.name = pokemonDetails.name
+    pokemon1.number = pokemonDetails.order
+    pokemon1.photo = pokemonDetails.sprites.other.dream_world.front_default
+
+    const types = pokemonDetails.types.map((typeSlots) => typeSlots.type.name)
+    const [type] = types
+    pokemon1.types = types
+    pokemon1.type = type
+
+    return pokemon1
+}
+
 pokeApi.getPokemonsDetails = (pokemons) => {
-    return fetch(pokemons.url).then((response) => response.json())
+    return fetch(pokemons.url)
+        .then((response) => response.json())
+        .then(convertPokemonDetailsToPokemonModel)
 }
 
 pokeApi.getPokemon = (offset = 0, limit = 5) => {
