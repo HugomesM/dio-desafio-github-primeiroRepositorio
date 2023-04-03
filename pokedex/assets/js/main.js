f247721ceb5a908e55c463764bc9576f296aafb7
HugogomesM <hugowork121@gmail.com>
2023-04-03 00:00:26 -0300

    separando e organizando o api da conversao html

--- pokedex/assets/js/main.js
+++ pokedex/assets/js/main.js
@@ -1,10 +1,6 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
 const pokemonList = document.getElementById("pokemonList");
 
-function convertPokemonToLi(pokemon){
-
+function convertPokemonToLi(pokemon) {
     return `
                 <li class="pokemon">
 
@@ -23,19 +19,6 @@ function convertPokemonToLi(pokemon){
                 </div>
             </li>
     `
-
 }
 
-
-
-fetch(url)
-    .then((response) => response.json())
-    .then((jsonBody) => jsonBody.results)
-    .then((pokemons)=>{
-        for (let i = 0; i < pokemons.length; i++) {
-            const pokemon = pokemons[i];
-            pokemonList.innerHTML += convertPokemonToLi(pokemon);
-            
-        }
-    })
-
+pokeApi.getPokemon().then((pokemons = []) => pokemonList.innerHTML += pokemons.map(convertPokemonToLi).join(""))
---
+++ pokedex/assets/js/poke-api.js
@@ -0,0 +1,8 @@
+const pokeApi = {}
+
+pokeApi.getPokemon = (offset = 0, limit = 0) => {
+    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+    return fetch(url)
+        .then((response) => response.json())
+        .then((jsonBody) => jsonBody.results)
+}
